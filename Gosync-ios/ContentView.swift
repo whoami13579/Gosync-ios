30eb0620fdecf9637c3bca3c1b6862c83ab693a9
whoami13579 <liangforcoding@gmail.com>
2024-05-31 09:35:01 +0800

    Initial Commit

---
+++ Gosync-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gosync-ios
+//
+//  Created by 褚翊喨 on 2024/5/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gosync-ios/Gosync_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gosync_iosApp.swift
+//  Gosync-ios
+//
+//  Created by 褚翊喨 on 2024/5/31.
+//
+
+import SwiftUI
+
+@main
+struct Gosync_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
